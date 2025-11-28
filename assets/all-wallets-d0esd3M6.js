244af48bb6968980768109e3a963a326a7965b49
Alex Shkor <alex.shkor@gmail.com>
2025-11-28 18:36:45 +0300

    Updates

--- assets/all-wallets-d0esd3M6.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-BNf0R8Jm.js";import"./index-7mnBBBHP.js";import"./index.es-C35_zGMv.js";import"./index-nibyPLVP.js";import"./fallback-BhE6yGwR.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{p as allWalletsSvg};
